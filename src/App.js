4fef8d6437c164f87c31b73a0da739c23e9d7e6a
Oron Evron <oronevron@gmail.com>
2019-03-25 18:26:41 +0200

    Added webpack, babel and react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <h1>blabla</h1>
+        );
+    }
+}
+
+export default App;
